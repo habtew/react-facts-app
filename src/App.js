5e5b3cd0ad412f35897b0db5220cbc56ea49ff2d
Habtamu <habtamuwolde92@gmail.com>
2023-04-23 21:38:59 +0530

    basic file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
     <h1>hello world</h1>
+    <Navbar />
+    </>
   );
 }
 
